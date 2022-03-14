a7f9abbd99dc8c0d66cdeda6246409de4a1137e2
Stephen Butler <data2fuse4butler@gmail.com>
2022-03-14 19:22:17 +0000

    Added git configuration script

---
+++ scripts/configureGit.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+git config --global user.email "data2fuse4butler@gmail.com"
+git config --global user.name "Stephen Butler"
+git config --global color.ui "auto"
+git config --global core.editor "vim"
